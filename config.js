261f3ffdf415870d7c30ea419fa6a54324c97c55
罗智毅 <luozhiyi@yaowutech.com>
2023-04-26 18:10:35 +0800

    feat: init

---
+++ config.js
@@ -0,0 +1,26 @@
+const port = 3001; // 端口
+const projectPath = "/Users/edy/Yaowu/g-united-web/dist"; // 指定项目地址
+// const projectPath = path.join(__dirname, '../dist');
+const entryHtml = "index.html"; // 入口html
+
+// 代理
+const proxyOptions = {
+  // target: "https://api.com", // target host
+  target: "http://xxx.com/api",
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    "^/api": "",
+    // "^/api/remove/path": "/path",
+  },
+  router: {
+    "dev.localhost:3000": "http://localhost:8000",
+  },
+};
+
+module.exports = {
+  port,
+  projectPath,
+  entryHtml,
+  proxyOptions,
+};
